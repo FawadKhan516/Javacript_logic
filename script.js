3468e68a15a2d4da635cf77f46aa55fb149bdb5d
Fawadkhan516 <sameerkhan100990@gmail.com>
2024-10-01 11:14:15 -0700

    javascript Logic

---
+++ script.js
@@ -0,0 +1,17 @@
+//  for each 
+ 
+ const array = ["rizwan", "ahad", "arqam", "faddi"];
+
+array.forEach(function(value){
+    if(value === "rizwan"){
+        let input = prompt('hello this is rizwan')
+        console.log(input)
+    }
+    else{
+        console.log(value)
+    }
+})
+
+
+
+// or ap for Each ma BREAK or CONTINUE nhi use kr sktay eror milay ga apko: 
